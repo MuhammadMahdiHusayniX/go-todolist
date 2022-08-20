028af6cb8d295c1309d0d8774745d84824cc61df
Mahdi <muhammad.mahdi.husaynix.bin.azizan@intel.com>
2022-08-21 01:01:34 +0800

    implement google authentication

--- go.mod
+++ go.mod
@@ -3,26 +3,36 @@ module github.com/MuhammadMahdiHusayniX/go-todolist
 go 1.19
 
 require (
+	cloud.google.com/go v0.67.0 // indirect
+	github.com/gin-contrib/sessions v0.0.5 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/goccy/go-json v0.9.10 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/markbates/goth v1.73.0 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
-	github.com/mattn/go-sqlite3 v1.14.15 // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.3 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/sqlite v1.3.6 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"strconv"
 
 	"github.com/MuhammadMahdiHusayniX/go-todolist/models"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
 )
+
+// var conf *oauth2.Config
+
+// User is a retrieved and authentiacted user.
+type User struct {
+	Sub           string `json:"sub"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Profile       string `json:"profile"`
+	Picture       string `json:"picture"`
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+	Gender        string `json:"gender"`
+}
 
 func init() {
 	models.Setup()
@@ -15,15 +37,15 @@ func AddTodo(c *gin.Context) {
 	todo := models.Todo{}
 
 	if err := c.BindJSON(&todo); err != nil {
-		c.JSON(400, err)
+		c.JSON(http.StatusBadRequest, err)
 	}
 
 	todoId, err := models.AddTodo(todo.Task, todo.CreatedBy)
 	if err != nil {
-		c.JSON(400, err)
+		c.JSON(http.StatusBadRequest, err)
 	}
 
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"todoId": todoId,
 	})
 }
@@ -31,15 +53,15 @@ func AddTodo(c *gin.Context) {
 func DeleteTodo(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.JSON(400, err)
+		c.JSON(http.StatusBadRequest, err)
 	}
 
 	deleteErr := models.DeleteTodo(id)
 	if deleteErr != nil {
-		c.JSON(400, deleteErr)
+		c.JSON(http.StatusBadRequest, deleteErr)
 	}
 
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"Successfully delete todo id: ": id,
 	})
 }
@@ -47,33 +69,121 @@ func DeleteTodo(c *gin.Context) {
 func RetrieveAll(c *gin.Context) {
 	todos, err := models.GetTodos()
 	if err != nil {
-		c.JSON(400, err)
+		c.JSON(http.StatusBadRequest, err)
 	}
 
-	c.JSON(200, todos)
+	c.JSON(http.StatusOK, todos)
 }
 
 func MarkComplete(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.JSON(400, err)
+		c.JSON(http.StatusBadRequest, err)
 	}
 
 	markCompleteError := models.MarkComplete(id)
 	if markCompleteError != nil {
-		c.JSON(400, markCompleteError)
+		c.JSON(http.StatusBadRequest, markCompleteError)
 	}
 
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"Successfully mark complete for id: ": id,
 	})
 }
 
+func IndexHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+}
+
+// LoginHandler handles the login procedure.
+func LoginHandler(c *gin.Context) {
+	q := c.Request.URL.Query()
+	q.Add("provider", "google")
+	c.Request.URL.RawQuery = q.Encode()
+	gothic.BeginAuthHandler(c.Writer, c.Request)
+	// session := sessions.Default(c)
+	// session.Set("state", "state")
+	// err := session.Save()
+	// if err != nil {
+	// 	c.HTML(http.StatusInternalServerError, "error.tmpl", gin.H{"message": "Error while saving session."})
+	// 	return
+	// }
+	// link := getLoginURL(state)
+	// c.HTML(http.StatusOK, "auth.tmpl", gin.H{"link": link})
+}
+
+func AuthHandler(c *gin.Context) {
+	q := c.Request.URL.Query()
+	q.Add("provider", "google")
+	c.Request.URL.RawQuery = q.Encode()
+	user, err := gothic.CompleteUserAuth(c.Writer, c.Request)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	// Handle the exchange code to initiate a transport.
+	session := sessions.Default(c)
+	session.Set("user-id", user.Email)
+	err = session.Save()
+	if err != nil {
+		log.Println(err)
+		c.HTML(http.StatusBadRequest, "error.tmpl", gin.H{"message": "Error while saving session. Please try again."})
+		return
+	}
+
+	c.HTML(http.StatusOK, "authenticated.tmpl", gin.H{
+		"user": user,
+	})
+}
+
+// AuthorizeRequest is used to authorize a request for a certain end-point group.
+func AuthorizeRequest() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		v := session.Get("user-id")
+		if v == nil {
+			c.HTML(http.StatusUnauthorized, "error.tmpl", gin.H{"message": "Please login."})
+			c.Abort()
+		}
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/todo", RetrieveAll)
-	r.GET("/todo/complete/:id", MarkComplete)
-	r.POST("/todo", AddTodo)
-	r.DELETE("/todo/:id", DeleteTodo)
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	store := cookie.NewStore([]byte("secret"))
+	store.Options(sessions.Options{
+		Path:     "/",
+		MaxAge:   86400 * 7,
+		HttpOnly: true,
+		Secure:   false,
+	})
+
+	gothic.Store = store
+
+	goth.UseProviders(
+		google.New("154301927301-uln6u2mc1ie0ojop9ig0mp0no3bcc776.apps.googleusercontent.com", "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", "http://127.0.0.1:3000/auth/google/callback"),
+	)
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.Use(sessions.Sessions("goquestsession", store))
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", IndexHandler)
+	r.GET("/login", LoginHandler)
+	r.GET("/auth/google/callback", AuthHandler)
+
+	authorized := r.Group("/todo")
+	authorized.Use(AuthorizeRequest())
+	{
+		authorized.GET("", RetrieveAll)
+		authorized.GET("/complete/:id", MarkComplete)
+		authorized.POST("", AddTodo)
+		authorized.DELETE("/:id", DeleteTodo)
+	}
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080
 }
